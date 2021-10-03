dc43e5cdb57e31161ac0f3b07aa98d0a2103f961
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-03 18:35:21 +0530

    Update __init__.py

--- unzipper/__init__.py
+++ unzipper/__init__.py
@@ -8,7 +8,7 @@ from config import Config
 
 plugins = dict(root="unzipper/modules")
 unzipperbot = Client(
-        "UnzipperBot",
+        "TG-UnzipperBot",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
