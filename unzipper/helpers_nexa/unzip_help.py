e1e529ca8ba42743e1873cad027f142ad46dfa35
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-03 19:27:37 +0530

    Update unzip_help.py

--- unzipper/helpers_nexa/unzip_help.py
+++ unzipper/helpers_nexa/unzip_help.py
@@ -12,7 +12,8 @@ from config import Config
 async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
+    #if round(diff % 10.00) == 0 or current == total:
+    if round(diff % 3.00) > 2.99 or current == total:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
@@ -21,20 +22,22 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
 
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        time_to_completion = TimeFormatter(milliseconds=time_to_completion) #time left
 
-        progress = "[{0}{1}] \n**Process**: {2}%\n".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
+        progress = "\n<b>Uploading......</b> \n{0}{1} <b>{2}%</b>\n".format(
+            ''.join(["●" for i in range(math.floor(percentage / 5))]),
+            ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n**Speed:** {2}/s\n**ETA:** {3}\n".format(
+        tmp = progress + "\n🍿 **UpLoaded : {0}\n🍿 File Size : {1}**\n🍿 **Speed:** {2}/s\n🍿 **Time Left :** {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            estimated_total_time if estimated_total_time != '' else "0 s"
+            #estimated_total_time if estimated_total_time != '' else "0 s"
+            time_to_completion if time_to_completion != '' else  "0 s" #time left
         )
         try:
-            await message.edit(text="{}\n {} \n\n**Powered by @NexaBotsUpdates**".format(ud_type,tmp))
+            await message.edit(text="{}\n {} \n\n**Made wit♥️by @MyTestBotZ**".format(tmp, ud_type))
         except:
             pass
 
@@ -75,7 +78,7 @@ def check_logs():
                 print("TF? Chat is not private")
                 return
             else:
-                client.send_message(chat_id=Config.LOGS_CHANNEL, text="`Unzipper-Bot has Successfully Started!` \n\n**Powered by @NexaBotsUpdates**")
+                client.send_message(chat_id=Config.LOGS_CHANNEL, text="`Unzipper-Bot has Successfully Started!` \n\n**Powered by @MyTestBotz**")
         else:
             print("No Log Channel ID is Given! Imma leaving Now!")
             exit()
